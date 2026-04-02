c59286033e783b66aac2c36639b4dcde5feb86f2
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-04-02 20:14:32 +0700

    first commit

---
+++ sortingMain08.java
@@ -0,0 +1,12 @@
+public class sortingMain08 {
+    public static void main(String[] args) {
+
+        int a[] = {20, 10, 2, 7, 12};
+        sorting08 dataurut1 = new sorting08(a, a.length);
+        System.out.println("Data awal 1:");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC):");
+        dataurut1.tampil();
+    }
+}
