88bf138bbc54457c40b101e9311872bac8fa41f4
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-04-02 20:33:08 +0700

    Insertion Sort

--- sortingMain08.java
+++ sortingMain08.java
@@ -16,5 +16,13 @@ public class sortingMain08 {
         dataurut2.selectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC):");
         dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        sorting08 dataurut3 = new sorting08(c, c.length);
+        System.out.println("Data awal 3:");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC):");
+        dataurut3.tampil();
     }
 }
