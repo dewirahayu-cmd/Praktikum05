bab01aff3266da4f17c1f763107249f32d2eaf8b
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-04-02 21:21:25 +0700

    Insertion Sort - DESC

--- mahasiswaBerprestasi08.java
+++ mahasiswaBerprestasi08.java
@@ -58,5 +58,19 @@ public class mahasiswaBerprestasi08 {
             listMhs[j] = temp;
         }
     }
+
+
+  // Insertion Sort - DESC berdasarkan IPK (Jawaban 5.4.3)
+    void insertionSortDesc() {
+        for (int i = 1; i < listMhs.length; i++) {
+            mahasiswa08 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
 }
  
