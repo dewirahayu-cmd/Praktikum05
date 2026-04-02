ab4dbe5fb9f0119f74c410b21a05524172d24145
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-04-02 20:32:53 +0700

    Insertion Sort

--- sorting08.java
+++ sorting08.java
@@ -23,7 +23,7 @@ public class sorting08 {
         }
     }
 
-     void selectionSort() {
+    void selectionSort() {
         for (int i = 0; i < jumData - 1; i++) {
             int min = i;
             for (int j = i + 1; j < jumData; j++) {
@@ -37,6 +37,17 @@ public class sorting08 {
         }
     }
 
+    void insertionSort() {
+        for (int i = 1; i <= data.length - 1; i++) {
+            int temp = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+    }
 
     void tampil() {
         for (int i = 0; i < jumData; i++) {
