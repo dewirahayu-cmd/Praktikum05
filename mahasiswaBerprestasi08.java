ab9ae62c096414ba03ba58e81ec37b4e2678682a
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-04-10 09:14:03 +0700

    modify 2

--- mahasiswaBerprestasi08.java
+++ mahasiswaBerprestasi08.java
@@ -72,5 +72,39 @@ public class mahasiswaBerprestasi08 {
             listMhs[j] = temp;
         }
     }
-}
+
+        // Sequential Searching 
+        int sequentialSearching (double cari) {
+            int posisi = -1;
+            for (int j = 0; j < listMhs.length; j++) {
+                if (listMhs [j].ipk == cari) {
+                    posisi = j;
+                    break;
+                }
+            }
+            return posisi;
+        }
+
+        void tampilPoisi (double x, int pos) {
+            if (pos != -1) {
+                System.out.println("data mahasiswa dengan IPK :" + x + " ditemukkan pada indeks " +pos );
+            }
+            else {
+                System.out.println("data " + x + "tidak ditemukan");
+            }   
+            }
+
+        void tampilDataSearch (double x, int pos) {
+            if (pos != -1) {
+                System.out.println("nim/t : " + listMhs [pos].nim);
+                System.out.println("nama/t : " + listMhs [pos].nama);
+                System.out.println("kelas/t : " +listMhs [pos].kelas);
+                System.out.println("ipk/t : " +x);
+            } 
+            else {
+                System.out.println("Data mahasiswa dengan IPK " + x + "tidak ditemukkan");
+            }
+        }
+        }
+
  
